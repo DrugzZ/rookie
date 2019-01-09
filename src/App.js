0b8a8a8ef0edfa18c8355948ee68fe0ca05fa77f
alpacacino <alpacacino@yandex.ru>
2019-01-09 18:47:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
+import rightPaneBg from './right-pane-bg.png';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +8,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="App-logo">
+            <img src={logo} alt="logo" />
+          </div>
         </header>
+        <section className="Content-wrap">
+          <div className="Left-pane">
+            <div className="Hero-text">
+              <p className="hero-text-1">rookie</p>
+              <p className="hero-text-2">digital</p>
+              <h1 className="hero-desc">Цифровое маркетинговое агентство.</h1>
+            </div>
+            <div className="Contact-form">
+            <form className="email-form">
+              <input className="email-input" placeholder="Введите email" type="email" name="email" />
+              <button className="hero-button" type="submit">Обратная связь</button>
+            </form>
+            </div>
+          </div>
+          <div className="Right-pane">
+            <img src={rightPaneBg} className="Right-pane-img" alt="Rookie Digital Marketing Agency" />
+          </div>
+        </section>
       </div>
     );
   }
