ff89f7ea2335848250ca9ad8e1c4955259e80948
alpacacino <alpacacino@yandex.ru>
2019-01-17 17:58:44 +0300

    added email validation from public api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.png';
 import rightPaneBg from './right-pane-bg.png';
 import './App.css';
 
 class App extends Component {
+  state = {
+    email: "",
+    verified: false, 
+    sent: false
+  }
+
+  handleChange = e => {
+    let val = e.target.value;
+    this.setState({email: val})
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    axios.get(`https://apilayer.net/api/check?access_key=dc6708bbf55fd23edc3de5aa7829f771&email=${this.state.email}&smtp=1`)
+      .then(res => {
+        let {format_valid, smtp_check} = res.data;
+        return format_valid && smtp_check
+      })
+      .then(verified => {
+        if(verified){
+          console.log("Good address")
+        } else {
+          console.log("Bad address")
+        }
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,8 +46,20 @@ class App extends Component {
               <h1 className="hero-desc">Цифровое маркетинговое агентство.</h1>
             </div>
             <div className="Contact-form">
-              <input className="email-input" placeholder="Введите email" type="email" name="email" />
-              <button className="hero-button" type="submit">Обратная связь</button>
+              <input 
+                value={this.state.email}
+                onChange={this.handleChange}
+                className="email-input" 
+                placeholder="Введите email" 
+                type="email" 
+                name="email" 
+              />
+              <button 
+                onClick={this.handleClick}
+                className="hero-button" 
+                type="submit">
+                Обратная связь
+              </button>
             </div>
           </div>
           <div className="Right-pane">
